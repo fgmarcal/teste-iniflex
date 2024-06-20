fe2357a6b7780b3e7d878841c2c95501f07f307a
fgmarcal <felipegarciamarcal@gmail.com>
2024-06-20 14:25:36 -0300

    add FormatarValor test

---
+++ src/test/java/com/teste/felipe/iniflex/Utils/FormatarValorTest.java
@@ -0,0 +1,38 @@
+package com.teste.felipe.iniflex.Utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+public class FormatarValorTest {
+
+    @Test
+    void deveAlterarUmValorFormatadoComDuasCasasDepoisDaVirgula(){
+        BigDecimal valor = new BigDecimal(3000);
+        BigDecimal valor2 = new BigDecimal("3000");
+
+        String valorTestado = "3.000,00";
+
+        var resultado = new FormatarValor().formatar(valor);
+        var resultado2 = new FormatarValor().formatar(valor2);
+
+        assertEquals(resultado, valorTestado);
+        assertEquals(resultado2, valorTestado);
+    }
+
+    @Test
+    void deveFalharQuandoOValorInseridoNaoSejaValido(){
+        try {
+            new FormatarValor().formatar(new BigDecimal("3000,00"));
+            fail();
+        } catch (Exception e) {
+            assertEquals(e, e);
+        }
+        
+    }
+    
+}

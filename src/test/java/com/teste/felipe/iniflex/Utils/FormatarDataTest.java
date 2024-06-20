dace55b61f931f81c7d299782c5ec7d703afc181
fgmarcal <felipegarciamarcal@gmail.com>
2024-06-20 11:28:59 -0300

    create formatData tests

---
+++ src/test/java/com/teste/felipe/iniflex/Utils/FormatarDataTest.java
@@ -0,0 +1,42 @@
+package com.teste.felipe.iniflex.Utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+public class FormatarDataTest {
+
+    private final FormatarData formatarData = new FormatarData();
+
+
+    
+    @Test
+    void deveFormatarUmaDataLocalDateNoPadraoDDMMAAAAEmString(){
+        LocalDate dataCompleta = LocalDate.of(1986, 6, 23);
+        var dataString = new String("23/06/1986");
+        FormatarData dataFormatada = new FormatarData();
+        var dataLocalDate = dataFormatada.formatarDataLocalDateParaString(dataCompleta);
+        assertEquals(dataLocalDate, dataString);
+    }
+
+    @Test
+    void deveFormatarUmaStringDeDataEmDDMMAAParaLocalDate(){
+        LocalDate dataCompleta = LocalDate.of(1986, 6, 23);
+        var dataString = new String("23/06/1986");
+        FormatarData dataFormatada = new FormatarData();
+
+        var formatada = dataFormatada.formatarDataStringParaLocalDate(dataString);
+        assertEquals(formatada, dataCompleta);
+    }
+
+    @Test
+    public void naoDeveFormatarDataStringParaLocalDateComFormatoInvalido() {
+        String dataString = "1986/06/23";
+        assertThrows(java.time.format.DateTimeParseException.class, () -> {
+            formatarData.formatarDataStringParaLocalDate(dataString);
+        }, "Era esperado lançar uma exceção para o formato de data inválido.");
+    }
+
+}
